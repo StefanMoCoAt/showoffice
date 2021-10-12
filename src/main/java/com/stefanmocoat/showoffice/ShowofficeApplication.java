d736734540d78c162a61fb03be2717417bd73b38
StefanMo031178 <stefan@mo.co.at>
2021-10-12 15:53:26 +0200

    Start

---
+++ src/main/java/com/stefanmocoat/showoffice/ShowofficeApplication.java
@@ -0,0 +1,13 @@
+package com.stefanmocoat.showoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowofficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowofficeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stefanmocoat/showoffice/ShowofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefanmocoat.showoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowofficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
